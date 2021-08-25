4aedc3ee3afe40fb3f70e1c2b351f2337a879cca
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-25 21:15:16 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,163 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Oven Mitt',
+        price: 6.96,
+        image: './img/img2.png',
+        description: [
+            {
+                text: '100% Cotton.',
+            },
+            {
+                text: 'Heat resistant oven mitt.',
+            },
+            {
+                text: 'Unique opposable thumb construction for a tight grip.',
+            },
+            {
+                text: 'Right handed design.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Cutting Board',
+        price: 29.99,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Manufactured in the country of China.',
+            },
+            {
+                text: 'Items Color: Natural.',
+            },
+            {
+                text: 'Decoration: engraved.',
+            },
+            {
+                text: 'Handsome cutting board in a football design with lazer engraved team logo.',
+            },
+            {
+                text: 'Bamboo is a renewable, sustainable resource.',
+            },
+            {
+                text: 'Dark bamboo design with light bamboo inlays.',
+            },
+            {
+                text: 'Great for game day.',
+            },
+            {
+                text: 'Measures 15 by 8-3/4 inches.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Set of 2 Metal Bird Cage',
+        price: 64.90,
+        discount: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: 'The vintage design of this home décor brings a sense of the past to your home.',
+            },
+            {
+                text: 'This item is made of solid iron material for durability and long lasting.',
+            },
+            {
+                text: 'This elegant set of decorative birdcages feature a distressed cream iron finish.',
+            },
+            {
+                text: 'This set features dome-shaped bird cages that look great on tables or ceilings.',
+            },
+            {
+                text: 'Each item in this set measures 7”L x 7”W x 18”H, 9”L x 9”W x 21”H, weight 4.41 lbs.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Moisture Meter',
+        price: 98.90,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Measures',
+                text: 'Separate settings for drywall, masonry, softwood and hardwood.',
+            },
+            {
+                title: 'No drywall damage',
+                text: 'Non-marring sensing technology protects drywall.',
+            },
+            {
+                title: 'Easy to read',
+                text: 'LEDs clearly indicate moisture level.',
+            },
+            {
+                title: 'HI visibility screen',
+                text: 'Large 2 inch (50mm) backlit white on black screen providing easier readability from any angle.',
+            },
+            {
+                title: 'Find mold',
+                text: 'Stops mold before it starts with early detection of moisture; essential water damage tool.',
+            },
+            {
+                title: 'Ergonomic',
+                text: 'Improved ergonomic handle design conforms to your hand for a more comfortable user experience.',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png', './img/img6-3.png', './img/img6-4.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Steel Rollator Walker',
+        price: 119.98,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                text: 'Steel Rollator walker supports up to 350 lbs.',
+            },
+            {
+                text: '6 inch wheels. Seat dimensions: 12 x 12 inches.',
+            },
+            {
+                text: 'Comfortable padded seat and backrest allow users to sit if they become tired.',
+            },
+            {
+                text: 'Bag under seat provides additional storage.',
+            },
+            {
+                text: 'Rolling walker weighs 19 lbs and folds flat for easy storage and transportation.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'ADF Helmet',
+        price: 148.00,
+        discount: 95.50,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'Adjustable ratchet head band to custom fit your head and absorbent.',
+            },
+            {
+                text: 'Oversize sweatband for improved comfort.',
+            },
+            {
+                text: 'Processes stick, tig, pulsed tig, mig, pulsed mig, flux-cored, gouging.',
+            },
+            {
+                text: 'Lightweight helmet makes for ease of use.',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png', './img/img10-4.png'],
+    },
+]
